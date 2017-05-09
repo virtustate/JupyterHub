26a1628a6d8c71a84af47f31b2da23576f4406f4
virtustate <stevek@virtustate.com>
2017-05-09 14:22:49 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 touch /root/started
+
+pt-get update -q -y
+apt-get upgrade -q -y
+apt-get install -q -y ysstat nano
+
+conda update conda -q -y
+conda update --all -q -y
+conda install -c conda-forge jupyterhub jupyter_contrib_nbextensions
+
+touch /root/finished
