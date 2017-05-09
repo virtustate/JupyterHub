ad8b4ceeee592d7d5fac48d91efe1e78a46c7d8f
virtustate <stevek@virtustate.com>
2017-05-09 14:27:50 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ touch /root/started
 
 pt-get update -q -y
 apt-get upgrade -q -y
-apt-get install -q -y ysstat nano
+apt-get install -q -y sysstat nano
 
 conda update conda -q -y
 conda update --all -q -y
